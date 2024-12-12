147718be4f11dd3ae40f30e8c5de3b64b1ee4280
Andrew Annex <ama6fy@virginia.edu>
2024-12-12 12:55:54 -0800

    build now works

--- src/planet_crs_registry_client/__init__.py
+++ src/planet_crs_registry_client/__init__.py
@@ -1,10 +1,10 @@
 """A client library for accessing planet_crs_registry"""
 
-from .client import AuthenticatedClient, Client
-
-__version__='0.1.0'
+__version__ = '0.1.0'
 
 __all__ = (
     "AuthenticatedClient",
     "Client",
 )
+
+from .client import AuthenticatedClient, Client
